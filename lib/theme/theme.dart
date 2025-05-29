3939e3f9969c26813ca5c0d6189fd0a258f1cc87
Ruiwen Guan <ruiwenguan@gmail.com>
2025-05-28 23:06:20 -0700

    changed theme

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -3,19 +3,47 @@ import 'package:flutter/material.dart';
 final ThemeData myTheme = ThemeData(
   scaffoldBackgroundColor: const Color(0xFF3B3B3B),
   primaryColor: const Color(0xFFFF9100),
-  // cardColor: const Color(0xFF3C3C3C),
   textTheme: const TextTheme(
     titleLarge: TextStyle(
+      fontSize: 22,
+      fontWeight: FontWeight.bold,
+      color: Color(0xFFD5DBDC),
+    ),
+    titleMedium: TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.bold,
+      color: Color(0xFFD5DBDC),
+    ),
+    titleSmall: TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.bold,
+      color: Color(0xFFD5DBDC),
+    ),
+    bodyLarge: TextStyle(fontSize: 18, color: Color(0xFFD5DBDC)),
+    bodyMedium: TextStyle(fontSize: 16, color: Color(0xFFD5DBDC)),
+    bodySmall: TextStyle(fontSize: 14, color: Color(0xFFD5DBDC)),
+    headlineLarge: TextStyle(
+      fontSize: 32,
+      fontWeight: FontWeight.bold,
+      color: Color(0xFFD5DBDC),
+    ),
+    headlineMedium: TextStyle(
+      fontSize: 28,
+      fontWeight: FontWeight.bold,
+      color: Color(0xFFD5DBDC),
+    ),
+    // this is the sign in page text style
+    headlineSmall: TextStyle(
       fontSize: 24,
       fontWeight: FontWeight.bold,
       color: Color(0xFFD5DBDC),
     ),
-    bodyLarge: TextStyle(fontSize: 16, color: Color(0xFFD5DBDC)),
-    bodyMedium: TextStyle(fontSize: 14, color: Color(0xFFD5DBDC)),
-    bodySmall: TextStyle(fontSize: 12, color: Color(0xFFD5DBDC)),
   ),
   inputDecorationTheme: const InputDecorationTheme(
-    labelStyle: TextStyle(color: Color(0xFFFF9100)),
+    // changes the style of the input text
+    labelStyle: TextStyle(color: Color(0xFFD5DBDC)),
+    // text style when float
+    floatingLabelStyle: TextStyle(color: Color(0xFFFF9100)),
     hintStyle: TextStyle(color: Color(0xFFD5DBDC)),
     border: UnderlineInputBorder(
       borderSide: BorderSide(color: Color(0xFFFF9100)),
@@ -36,8 +64,23 @@ final ThemeData myTheme = ThemeData(
     ),
   ),
   textButtonTheme: TextButtonThemeData(
-    style: TextButton.styleFrom(foregroundColor: const Color(0xFFFF9100)),
+    style: TextButton.styleFrom(
+      foregroundColor: const Color(0xFFFF9100),
+      textStyle: TextStyle(color: Color(0xFFD5DBDC)),
+    ),
   ),
+  // experimenting colors of the buttons
+  // iconButtonTheme: IconButtonThemeData(
+  //   style: IconButton.styleFrom(foregroundColor: Color(0xFFFF9100)),
+  // ),
+  // filledButtonTheme: FilledButtonThemeData(
+  //   style: FilledButton.styleFrom(foregroundColor: const Color(0xFFFF9100)),
+  // ),
+  // this is the sign in button
+  outlinedButtonTheme: OutlinedButtonThemeData(
+    style: OutlinedButton.styleFrom(foregroundColor: Color(0xFFD5DBDC)),
+  ),
+
   appBarTheme: const AppBarTheme(
     backgroundColor: Color(0xFF3B3B3B),
     elevation: 0,
